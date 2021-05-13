9ee4fc4c8ed24b2598beb9503a8602a5eb61bd15
seema-web <seemamehta1123@gmail.com>
2021-05-13 23:09:42 +0530

    first commit

---
+++ movieproject/testapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    rdate=models.DateField()
+    moviename = models.CharField(max_length=30)
+    hero = models.CharField(max_length=30)
+    heroine = models.CharField(max_length=30)
+    rating = models.IntegerField()
